7dc19162973738bb0a18596cee92200f305661ea
Fizryan <52873306+Fizryan@users.noreply.github.com>
2025-03-10 02:43:27 +0700

    menambahkan implementasi dengan table-driven

---
+++ Semester-4/Praktikum_KPL/TPmod4_103022300158/TPmod4_103022300158/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Masukkan nama kelurahan: ");             // Menampilkan pesan input ke pengguna
+        string kelurahan = Console.ReadLine();                  // Menerima input kelurahan dari pengguna
+
+        string kodePos = KodePos.GetKodePos(kelurahan);         // Memanggil method untuk mendapatkan kode pos
+        Console.WriteLine($"Kode Pos {kelurahan}: {kodePos}");  // Menampilkan kode pos dari kelurahan yang dimasukkan
+    }
+}
